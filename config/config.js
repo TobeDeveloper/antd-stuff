885b7467dd5942d3d37163a48e25feccaae79d8c
myan <mryanhaoxd@163.com>
2019-06-10 21:40:53 +0800

    add sample codes

---
+++ config/config.js
@@ -0,0 +1,16 @@
+export default {
+    singular: true,
+    plugins: [
+        ['umi-plugin-react', {
+            antd: true
+        }],
+        ],
+    routes: [{
+        path: '/',
+        component: '../layout',
+        routes: [{
+            path: 'idx',
+            component: './Index'
+        }]
+    }]
+}
